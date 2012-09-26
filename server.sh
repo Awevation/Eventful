af70a6fde5c3bbd746bbf99efabca86f470e2728
Milo Mordaunt <milomord@gmail.com>
2012-09-26 23:46:43 +0100

    everything broken. So badly.

--- server.sh
+++ server.sh
@@ -3,9 +3,19 @@ if [ "$1" == "start" ]; then
     cd server
     echo "starting server..."
     node index.js >> log.txt&
+    if [ "$(pidof node)" != "" ]; then
+	echo "Great success!!"
+    else
+	echo "Oops, looks like an error occured. Probably maybe."
+    fi
 elif [ "$1" == "stop" ]; then
     echo "stopping server..."
     kill $(pidof node)
+    if [ "$(pidof node)" == "" ]; then
+	echo "Great success!!"
+    else
+	echo "Oops, looks like an error occured. Probably maybe."
+    fi
 elif [ "$1" == "check" ]; then
     if [ "$(pidof node)" != "" ]; then
 	echo "Server is running"
