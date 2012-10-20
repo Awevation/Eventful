29d39683c722a25afe6da75c91e6b17d2c24e37d
Milo Mordaunt <milomord@gmail.com>
2012-10-20 12:32:12 +0100

    I don't know.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
-handle["/postLonLat"] = requestHandlers.lonLat;
+handle["/postLonLat"] = requestHandlers.postLonLat;
 
 server.start(router.route, handle);
